62c991a1fc375c41f09d24af4dcf3976cdee653f
Lulu Pradeepan <lulupradeepan@gmail.com>
2025-08-13 09:54:53 +0530

    Add files via upload

---
+++ TwoDArray.java
@@ -0,0 +1,16 @@
+package Lab3;
+
+public class TwoDArray {
+	public static void main(String[]args) {
+		int[][]matrix= {{1,2,3},
+				        {5,6,7,8}};
+		for(int i=0;i<matrix.length;i++) {
+			for(int j=0;j<matrix[i].length;j++) {
+				System.out.print(matrix[i][j]+"\t");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
