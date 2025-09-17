04b3c23660f7e77223e9dbae669bf4cc0f6c4494
Lulu Pradeepan <lulupradeepan@gmail.com>
2025-09-17 10:47:44 +0530

    Add files via upload

---
+++ ExceptionDemo.java
@@ -0,0 +1,13 @@
+package Lab3;
+
+public class ExceptionDemo {
+	public static void main(String[]args) {
+		
+		String college="SJCET";
+		System.out.println(college.charAt(10));
+		int[]array= {1,2,3,4,5};
+		System.out.println(array[6]);
+		String s2="abc";
+		System.out.println(Integer.parseInt(s2));
+	}
+}
